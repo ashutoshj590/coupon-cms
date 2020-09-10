b87548f3f220a61c704a061f34c54ef01132157f
Ashutosh Joshi <ashutoshj590@gmail.com>
2020-09-10 23:32:57 +0530

    Add header in html

--- src/app/_constant.ts
+++ src/app/_constant.ts
@@ -3,10 +3,6 @@ export const apiUrl = {
     live: 'http://www.mccpapp.com:8080',
     server: 'http://www.mccpapp.com:8080',
     grant_type: "password",
-    // Local
-    // client_id: 2,
-    // client_secret: 'tbQXX4hloymBnokfhw0dzIFjAXQEZQTsWt52AmBc'
-    // Server
     client_id: 2,
     client_secret: 'bD9lO4jkLSCS6CeM45IEk9cgT1OHHg4XsR5b7lOM'
 }
